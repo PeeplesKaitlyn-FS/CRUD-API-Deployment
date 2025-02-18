c45656f6bc825fabd668badc2439d17e0c12e92b
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-02-17 23:58:32 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Movie = require('./models/Movie');
+const Movie = require('../models/Movie');
 const path = require('path');
 
 mongoose.connect('mongodb://localhost:27017/cinema-app', {
@@ -18,64 +18,70 @@ db.once('open', function() {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Serve the React app's static files
-app.use(express.static(path.join(__dirname, '../client/cinema-app/build')));
+console.log('Server started');
 
 // API routes
 app.get('/api/movies', (req, res) => {
+  console.log('Received request for movies');
   Movie.find().then(movies => {
+    console.log('Movies found:', movies);
     res.json(movies);
   }).catch(err => {
+    console.error('Error retrieving movies:', err);
     res.status(500).json({ message: 'Error retrieving movies' });
   });
 });
 
 app.get('/api/movies/:id', (req, res) => {
+  console.log('Received request for movie by id');
   Movie.findById(req.params.id).then(movie => {
     if (!movie) {
+      console.log('Movie not found');
       res.status(404).json({ message: 'Movie not found' });
     } else {
+      console.log('Movie found:', movie);
       res.json(movie);
     }
   }).catch(err => {
+    console.error('Error retrieving movie:', err);
     res.status(500).json({ message: 'Error retrieving movie' });
   });
 });
 
 app.post('/api/movies', (req, res) => {
+  console.log('Received request to create movie');
   const movie = new Movie(req.body);
   movie.save().then(movie => {
+    console.log('Movie created:', movie);
     res.json(movie);
   }).catch(err => {
+    console.error('Error creating movie:', err);
     res.status(400).json({ message: 'Error creating movie' });
   });
 });
 
 app.put('/api/movies/:id', (req, res) => {
+  console.log('Received request to update movie');
   Movie.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(movie => {
+    console.log('Movie updated:', movie);
     res.json(movie);
   }).catch(err => {
+    console.error('Error updating movie:', err);
     res.status(400).json({ message: 'Error updating movie' });
   });
 });
 
 app.delete('/api/movies/:id', (req, res) => {
+  console.log('Received request to delete movie');
   Movie.findByIdAndRemove(req.params.id).then(movie => {
+    console.log('Movie deleted:', movie);
     res.json({ message: 'Movie deleted successfully' });
   }).catch(err => {
+    console.error('Error deleting movie:', err);
     res.status(404).json({ message: 'Movie not found' });
   });
-});
-
-// Serve the React app's index.html file for the root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/cinema-app/build/index.html'));
-});
-
-// Serve the React app's index.html file for any unknown routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/cinema-app/build/index.html'));
 });
 
 app.listen(3000, () => {
