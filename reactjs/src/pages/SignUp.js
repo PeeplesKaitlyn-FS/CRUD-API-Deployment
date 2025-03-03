163a7f5fc50460481a5f1aada94c8846b94a6658
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-03 01:09:49 -0500

    Update SignUp.js

--- reactjs/src/pages/SignUp.js
+++ reactjs/src/pages/SignUp.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import "../App.css";
+import authService from "../services/auth.service";
 
 function SignUp() {
   const [email, setEmail] = useState("");
@@ -10,29 +11,45 @@ function SignUp() {
   const navigate = useNavigate();
 
   const handleSignup = async (event) => {
-    e.preventDefault();
+    event.preventDefault();
     try {
-      const response = await fetch("/api/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+      await authService.signup(email, password).then(
+        response => {
+          
+          navigate("/dashboard");
         },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await response.json();
-      console.log(data);
-      navigate("/login");
+        (error) => {
+          console.log(error);
+        }
+      ) 
     } catch (error) {
       console.log(error);
     }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>SignUp Page</h1>
         <Link to="/dashboard">Dashboard</Link>
-
       </header>
+      <form onSubmit={handleSignup}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   );
 }
