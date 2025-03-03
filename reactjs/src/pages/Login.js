1928c2cbe6f5d542a6241ca8b250d64a75aa7d2b
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-03 01:09:46 -0500

    Update Login.js

--- reactjs/src/pages/Login.js
+++ reactjs/src/pages/Login.js
@@ -1,14 +1,57 @@
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import "../App.css";
+import authService from "../services/auth.service";
+
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleLogin = async (event
+    ) => {
+    event.preventDefault();
+    try {
+        await authService.login(email, password).then(
+            response => {
+                navigate("/dashboard");
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    } catch (error) {
+        console.log(error);
+    }
+    };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>SignUp Page</h1>
+        <h1>Login Page</h1>
         <Link to="/dashboard">Dashboard</Link>
       </header>
+      <section>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+        </section>
     </div>
   );
 }
